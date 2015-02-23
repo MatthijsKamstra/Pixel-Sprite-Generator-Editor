1f3a3b699d6551ea8b274016673779b1b5a2f43c
Matthijs Kamstra aka [mck] <matthijs@fonkmobile.nl>
2015-02-23 21:54:26 +0100

    added getter/setters

--- Lib/psg/Mask.hx
+++ Lib/psg/Mask.hx
@@ -4,11 +4,56 @@ package psg;
 class Mask 
 {
 
-	public var data:Array<Int>;
-	public var width:Int;
-	public var height:Int;
-	public var mirrorX:Bool;
-	public var mirrorY:Bool;
+	public var data(get_data, set_data):Array<Int>;
+	private var _data:Array<Int>;
+
+	function get_data():Array<Int> { return _data; }
+	function set_data(value:Array<Int>):Array<Int>
+	{
+		return _data = value;
+	}
+
+	public var width(get_width, set_width):Int;
+	private var _width:Int;
+	
+	function get_width():Int { return _width; }
+	function set_width(value:Int):Int
+	{
+		return _width = value;
+	}
+
+	public var height(get_height, set_height):Int;
+	private var _height:Int;
+	
+	function get_height():Int { return _height; }
+	function set_height(value:Int):Int
+	{
+		return _height = value;
+	}
+
+	public var mirrorX(get_mirrorX, set_mirrorX):Bool;
+	private var _mirrorX:Bool;
+	
+	function get_mirrorX():Bool { return _mirrorX; }
+	function set_mirrorX(value:Bool):Bool
+	{
+		return _mirrorX = value;
+	}
+
+	public var mirrorY(get_mirrorY, set_mirrorY):Bool;
+	private var _mirrorY:Bool;
+	
+	function get_mirrorY():Bool { return _mirrorY; }
+	function set_mirrorY(value:Bool):Bool
+	{
+		return _mirrorY = value;
+	}
+
+	// public var data:Array<Int>;
+	// public var width:Int;
+	// public var height:Int;
+	// public var mirrorX:Bool;
+	// public var mirrorY:Bool;
 
 	/**
 	*   The Mask class defines a 2D template form which sprites can be generated.
